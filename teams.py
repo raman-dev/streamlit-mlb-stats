f337c70fbc21f602aca62e49fa82551dee54e2ec
raman-dev <ramandeep.dev582@gmail.com>
2025-05-13 14:07:42 -0400

    table view of teams with record and hit stats

--- teams.py
+++ teams.py
@@ -54,6 +54,70 @@ st.selectbox(
     format_func=lambda x: x['name']
 )
 
+@st.cache_data
+def getStatDict(statGroup,teamId):
+    return statsapi.get("team_stats",
+                      params={
+                          'season':SEASON,
+                          'stats':'season',
+                          'teamId':teamId,
+                          'group':statGroup})['stats'][0]['splits'][0]['stat']
+
+
+def get_stats(statGroup,statNames,teamId):
+    statDict = getStatDict(statGroup,teamId)
+    return [ statDict[x] for x in statNames ]
+
+
+@st.fragment
+def showTeamsWithStats():
+
+    """
+        write as table
+    """
+    table = []
+    standings = statsapi.standings_data(season='2025')
+    # st.write(standings)
+    # """
+        # standings -> {
+        #     div_id
+        #     teams [
+        #         team_data_dict
+        #     ]
+        # }
+         
+    # """
+    teamStandingDict = {}
+    for div_id,div_dict in standings.items():
+        # st.write(div_dict)
+        for team_dict in div_dict['teams']:
+            # st.write(team_dict)
+            teamStandingDict[team_dict['team_id']] = team_dict
+        
+    # st.write(teamStandingDict) 
+    for d in teamIds:
+        name = d['name']
+        id = d['id']
+
+        
+        teamStanding = teamStandingDict[id]
+        
+        hittingStats = get_stats('hitting',['hits','rbi','gamesPlayed'],id)
+        pitchingStats = get_stats('pitching',['hits','runs'],id)
+        # winsLosses = get_stats('pitching',['wins','losses'],id)
+        
+        table.append([int(teamStanding['sport_rank'])] + 
+                     [name] + 
+                     [  teamStanding['w'],
+                         teamStanding['l']] +
+                     hittingStats)
+
+    columns = ["Rank","team","wins","losses" ,"hits","rbi","gamesPlayed"]
+
+    df = pd.DataFrame(data=table,columns=columns)
+    st.dataframe(df,hide_index=True)
+
+showTeamsWithStats()
 # leagueLeaderTypes = statsapi.meta('leagueLeaderTypes')
 # st.selectbox(
 #     "League Leader Types",
@@ -61,17 +125,11 @@ st.selectbox(
 #      key='leagueLeaderTypes',
 #      format_func=lambda x: x['displayName'].title()
 # )
+"""
+`{t["w"]}`
 
+"""
 
-@st.cache_data
-def get_stats(statGroup,statNames):
-    statDict = statsapi.get("team_stats",
-                      params={
-                          'season':SEASON,
-                          'stats':'season',
-                          'teamId':st.session_state['team']['id'],
-                          'group':statGroup})['stats'][0]['splits'][0]['stat']
-    return [ statDict[x] for x in statNames ]
 
 
 @st.cache_data
@@ -84,6 +142,9 @@ def gamesToday(team_id,date):
     return result
 """
     get the games today for this team
+
+    stats only mean something in reference to others
+    view all 
 """
 
 # st.write(st.session_state['team'])
@@ -96,72 +157,72 @@ team_id = st.session_state['team']['id']
 #                           season=SEASON
 #                           ))
 
-current,other = st.columns(2)
+# current,other = st.columns(2)
 
-result = gamesToday(st.session_state['team']['id'],today_str)[0]
+# result = gamesToday(st.session_state['team']['id'],today_str)[0]
 
-if result != []:
-    """
-        grab team names 
-    """
-    current_teamName = result['home_name']
-    other_teamName = result['away_name']
-    other_teamId = result['away_id']
-    if result['home_id'] != team_id:
-        #current team is not home team
-        current_teamName = result['away_name']
-        other_teamName = result['home_name']
-        other_teamId = result['home_id']
+# if result != []:
+#     """
+#         grab team names 
+#     """
+#     current_teamName = result['home_name']
+#     other_teamName = result['away_name']
+#     other_teamId = result['away_id']
+#     if result['home_id'] != team_id:
+#         #current team is not home team
+#         current_teamName = result['away_name']
+#         other_teamName = result['home_name']
+#         other_teamId = result['home_id']
 
-    # current.write(current_teamName)
-    # other.write(other_teamName)
-    st.header(current_teamName)
+#     # current.write(current_teamName)
+#     # other.write(other_teamName)
+#     st.header(current_teamName)
     
-    columns = ["Hits Allowed","Runs Allowed","RBIs","Hits","Games Played"]
+#     columns = ["Hits Allowed","Runs Allowed","RBIs","Hits","Games Played"]
 
-    hittingStats = get_stats('hitting',['hits','rbi','gamesPlayed'])
-    pitchingStats = get_stats('pitching',['hits','runs'])
-    winsLosses = get_stats('pitching',['wins','losses'])
+#     hittingStats = get_stats('hitting',['hits','rbi','gamesPlayed'],st.session_state['team']['id'])
+#     pitchingStats = get_stats('pitching',['hits','runs'],st.session_state['team']['id'])
+#     winsLosses = get_stats('pitching',['wins','losses'],st.session_state['team']['id'])
     
-    st.write(winsLosses)
-    data = [pitchingStats + hittingStats]
-    df = pd.DataFrame(data=data,columns=columns)
-    df_t = df.T.copy()
-    df_t.columns = ["Value"]
-    st.dataframe(df_t)
-    # st.dataframe(df,hide_index=True)
+#     st.write(winsLosses)
+#     data = [pitchingStats + hittingStats]
+#     df = pd.DataFrame(data=data,columns=columns)
+#     df_t = df.T.copy()
+#     df_t.columns = ["Value"]
+#     st.dataframe(df_t)
+#     # st.dataframe(df,hide_index=True)
     
-    # """
-    #     statGroups:
-    #         hitting
-    #             hits,atBats,rbis
-    #         pitching
-    #             hits, numberOfPitches
-    #         fielding
-    # """
-    st.header(other_teamName)
-    # standings = statsapi.standings_data(season='2025')
-    #returns map split by division
-    #div_id : {
-    #   div_name:
-    #   team_id_0: data,
-    #   team_id_1: data,
-    #   ...
-    # #}
-    # st.write(standings)
-    # for div_id,div_data in standings.items():
-    #     # st.write(d)
-    #     div_name = div_data['div_name']
-    #     table = []
-    #     # st.write(div_name)
-    #     for team_dict in div_data['teams']:
-    #         row = {}
-    #         if team_dict['team_id'] == team_id:
-    #             # row['team_id'] = t['team_id']
-    #             row['name']= team_dict['name']
-    #             row['wins'] = f'`{team_dict["w"]}`'
-    #             row['losses'] = team_dict['l']
-    #             table.append(row)
-    #             st.table(table)
-else:
-    st.write("No games today")
+#     # """
+#     #     statGroups:
+#     #         hitting
+#     #             hits,atBats,rbis
+#     #         pitching
+#     #             hits, numberOfPitches
+#     #         fielding
+#     # """
+#     st.header(other_teamName)
+#     # standings = statsapi.standings_data(season='2025')
+#     #returns map split by division
+#     #div_id : {
+#     #   div_name:
+#     #   team_id_0: data,
+#     #   team_id_1: data,
+#     #   ...
+#     # #}
+#     # st.write(standings)
+#     # for div_id,div_data in standings.items():
+#     #     # st.write(d)
+#     #     div_name = div_data['div_name']
+#     #     table = []
+#     #     # st.write(div_name)
+#     #     for team_dict in div_data['teams']:
+#     #         row = {}
+#     #         if team_dict['team_id'] == team_id:
+#     #             # row['team_id'] = t['team_id']
+#     #             row['name']= team_dict['name']
+#     #             row['wins'] = f'`{team_dict["w"]}`'
+#     #             row['losses'] = team_dict['l']
+#     #             table.append(row)
+#     #             st.table(table)
+# else:
+#     st.write("No games today")
