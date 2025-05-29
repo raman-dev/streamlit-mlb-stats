c952e2148a9fa24278283cfc091b3ce52c6a71f2
raman-dev <ramandeep.dev582@gmail.com>
2025-05-29 12:24:31 -0400

    meh

---
+++ models.py
@@ -0,0 +1,55 @@
+# st.write(x)
+# """
+#     returns a dict
+#         keys
+#             innings : [
+#                 9 innings data
+#                     ith_{
+#                         home {
+#                             runs
+#                             }
+#                     }
+#             ]
+#     game_id: {
+#         home {
+#             total_runs
+#             total_hits
+#             0 : r,h
+#             1 : r,h
+#             2 :
+#         }
+#         away {
+#             total_runs
+#             total_hits
+#         }
+#     }
+# """
+# game
+#     id
+#     home_id
+#     away_id
+#     home
+#         runs
+#         hits
+#     away
+#         runs
+#         hits
+#     innings
+#       runs:
+#         h : [0,0,0,0,0,0,0,0,0],
+#         a : [0,0,0,0,0,0,0,0,0]
+#       hits:
+#         h : [0,0,0,0,0,0,0,0,0],
+#         a : [0,0,0,0,0,0,0,0,0]
+
+# team_histogram
+#     team_id
+#     runs : [0,0,0,0,0,0,0,0,0]
+#     hits: [0,0,0,0,0,0,0,0,0]
+#     runs_allowed: [0,0,0,0,0,0,0,0,0]
+#     hits_allowed: [0,0,0,0,0,0,0,0,0]
+# """
+
+
+
+
--- teams.py
+++ teams.py
@@ -186,57 +186,7 @@ def gamesPlayed(results):
     # x = statsapi.get("game_linescore",params={
     #     'gamePk':id
     # })
-    # st.write(x)
-    # """
-    #     returns a dict
-    #         keys
-    #             innings : [
-    #                 9 innings data
-    #                     ith_{
-    #                         home {
-    #                             runs                
-    #                             }
-    #                     }
-    #             ]
-    #     game_id: {
-    #         home {
-    #             total_runs
-    #             total_hits
-    #             0 : r,h
-    #             1 : r,h
-    #             2 : 
-    #         }
-    #         away {
-    #             total_runs
-    #             total_hits
-    #         }
-    #     }
-    # """
-    # game
-    #     id
-    #     home_id
-    #     away_id
-    #     home 
-    #         runs
-    #         hits
-    #     away 
-    #         runs
-    #         hits 
-    #     innings 
-    #       runs:
-    #         h : [0,0,0,0,0,0,0,0,0],
-    #         a : [0,0,0,0,0,0,0,0,0]
-    #       hits:
-    #         h : [0,0,0,0,0,0,0,0,0],
-    #         a : [0,0,0,0,0,0,0,0,0]
-    
-    # team_histogram
-    #     team_id
-    #     runs : [0,0,0,0,0,0,0,0,0]
-    #     hits: [0,0,0,0,0,0,0,0,0]
-    #     runs_allowed: [0,0,0,0,0,0,0,0,0]
-    #     hits_allowed: [0,0,0,0,0,0,0,0,0]
-    # """
+
     # """
     # break
         data.append([
@@ -259,5 +209,5 @@ hits = [2,4,6,7,12,4,2,1,0.5]
 runs = [4,3,1,3,5,6,8,9,1]
 
 df = pd.DataFrame(data={"hits per inning":hits,"runs per inning":runs})
-# st.write(df)
-st.bar_chart(data=df)
+st.subheader("Hits/Runs Per Inning")
+st.bar_chart(data=df,color=["#fd0","#f0f"])
